f9346784d15845188d11fc77c646d6973bc454e7
Francesco Paolocci <micky5991@gmail.com>
2021-10-23 13:30:17 +0200

    Setup tests for questfactory

---
+++ Quests.Tests/QuestFactoryFixture.cs
@@ -0,0 +1,57 @@
+using System;
+using FluentAssertions;
+using Micky5991.EventAggregator.Interfaces;
+using Micky5991.EventAggregator.Services;
+using Micky5991.Quests.Interfaces.Nodes;
+using Micky5991.Quests.Services;
+using Micky5991.Quests.Tests.TestBases;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Serilog;
+using Serilog.Core;
+
+namespace Micky5991.Quests.Tests;
+
+[TestClass]
+public class QuestFactoryFixture : QuestTestBase
+{
+    private QuestFactory factory;
+
+    private Mock<IQuestRootNode> quest;
+
+    private IServiceProvider serviceProvider;
+
+    [TestInitialize]
+    public void Setup()
+    {
+        this.quest = new Mock<IQuestRootNode>();
+
+        this.serviceProvider = new ServiceCollection()
+                               .AddSingleton<IEventAggregator, EventAggregatorService>()
+                               .AddTransient(x => this.quest.Object)
+                               .AddLogging(builder => builder.AddSerilog(Logger.None))
+                               .BuildServiceProvider();
+
+        this.factory = new QuestFactory(this.serviceProvider);
+    }
+
+    [TestCleanup]
+    public void Cleanup()
+    {
+        this.quest = null;
+
+        this.serviceProvider = null;
+    }
+
+    [TestMethod]
+    public void QuestFactoryInitializesQuest()
+    {
+        this.quest.Setup(x => x.Initialize());
+
+        var createdQuest = this.factory.BuildQuest<IQuestRootNode>();
+
+        createdQuest.Should().BeSameAs(this.quest.Object);
+        this.quest.Verify(x => x.Initialize(), Times.Once);
+    }
+}
